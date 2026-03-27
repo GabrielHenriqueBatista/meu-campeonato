c00bc87c39c815184a1d274f7707086ab9a7b746
GabrielHenriqueBatista <gabrielhenriquebatista31@gmail.com>
2026-03-27 00:10:28 -0300

    feat(quality): [CARD-02] configurar husky hooks e analise estatica

--- Makefile
+++
@@ -1,3 +0,0 @@
-install-hooks:
-	cp scripts/pre-commit .git/hooks/pre-commit
-	chmod +x .git/hooks/pre-commit
---
+++ commitlint.config.js
@@ -0,0 +1,12 @@
+export default {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            ['feat', 'fix', 'chore', 'docs', 'test', 'refactor', 'style', 'ci'],
+        ],
+        'subject-max-length': [2, 'always', 100],
+        'body-max-line-length': [0, 'always'],
+    },
+};
